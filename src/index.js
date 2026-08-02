4663e72b0536c165de71dd94fb20de3a92876619
Shubham <sjangid@9820gmail.com>
2026-08-02 13:46:37 +0530

    Setup the CORS and created uitilty for the async handler , error handler and response handler

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import dotenv from 'dotenv';
 import connectDB from './db/index.js';
+import { app } from './app.js';
 
 dotenv.config({
     path: './env'
 });
 
-connectDB();
+const port = process.env.PORT || 8000;
+
+connectDB()
+.then((response) => {
+    app.listen(port, () => {
+        console.log('Server is running on port: ', port);
+    })
+
+    app.on('error', (error) => {
+        console.log('Error', error);
+        throw(error);
+    })
+})
+.catch((err) => {
+    console.log('Error connecting the database', err);
+});
