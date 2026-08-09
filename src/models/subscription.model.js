9a54bd194d31113c050ef63ca8e808d3edf1cade
Shubham <sjangid9820@gmail.com>
2026-08-09 13:20:59 +0530

    Created the subscription model

---
+++ src/models/subscription.model.js
@@ -0,0 +1,17 @@
+import {mongoose, Schema} from 'mongoose';
+
+const subscriptionSchema = new Schema({
+    subscriber: {
+        type: Schema.Types.ObjectId, // Subscroiber is the user who is subscribing to another user
+        ref: 'User',
+        required: true
+    },
+    subscribedTo: {
+        typer: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }
+}, {timestamps: true});
+
+const Subscription = mongoose.model('Subscription', subscriptionSchema);
+
